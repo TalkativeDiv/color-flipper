1fdf4836217c09e2e6e0d3d9691478c17b7d3764
TalkitiveDiv <abhipieznomnomnom@gmail.com>
2021-07-26 13:44:00 -0400

    revamped ui + more

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 const btnRandomColor = document.getElementById('btnRandomColor');
 const spnColor = document.getElementById('spnColor');
-
 // thanks stack overflow 😎
 let getRandomInt = (min, max) => {
      min = Math.ceil(min);
      max = Math.floor(max);
      return Math.floor(Math.random() * (max - min + 1)) + min;
  }
-
-
- let componentToHex = (c) => {
+let componentToHex = (c) => {
      var hex = c.toString(16);
      return hex.length == 1 ? "0" + hex : hex;
    }
-   
-
- let rgbToHex = (r, g, b) => {
+let rgbToHex = (r, g, b) => {
      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
    }
-
-   let getRandomColor = () =>{
-    var redVal = getRandomInt(0,255);
-    var greenVal = getRandomInt(0,255);
-    var blueVal = getRandomInt(0,255);
-   var randomColor =  rgbToHex(redVal, greenVal, blueVal);
-    spnColor.innerHTML = randomColor
-    spnColor.style = 'color:' + randomColor
-   }
-
-window.addEventListener('DOMContentLoaded',() =>{
-  getRandomColor()
-})
-
-btnRandomColor.addEventListener('click', () =>{
-  getRandomColor()
-})
+let getRandomColor = () =>{
+   var randomColor =  rgbToHex(getRandomInt(0,255), getRandomInt(0,255), getRandomInt(0,255));
+    spnColor.innerHTML = randomColor;
+    spnColor.style = 'color:' + randomColor;
+}
+window.addEventListener('DOMContentLoaded',getRandomColor());
+btnRandomColor.addEventListener('click', getRandomColor());
