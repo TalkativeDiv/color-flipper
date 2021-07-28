6b375471d713dc87483e1d7fc04e40ae35c44a40
TalkitiveDiv <abhipieznomnomnom@gmail.com>
2021-07-28 18:39:21 -0400

    added repo button + more

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,16 @@
+//variables
 const btnRandomColor = document.getElementById('btnRandomColor');
 const spnColor = document.getElementById('spnColor');
-// thanks stack overflow 😎
+const btnRepo = document.getElementById('btnRepo');
+const ghRepo = 'https://github.com/TalkativeDiv/color-flipper';
+//functions
+//getting the random integer
 let getRandomInt = (min, max) => {
      min = Math.ceil(min);
      max = Math.floor(max);
      return Math.floor(Math.random() * (max - min + 1)) + min;
  }
+ //rgb to hex
 let componentToHex = (c) => {
      var hex = c.toString(16);
      return hex.length == 1 ? "0" + hex : hex;
@@ -13,10 +18,19 @@ let componentToHex = (c) => {
 let rgbToHex = (r, g, b) => {
      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
    }
+//applying all of these functions
 let getRandomColor = () =>{
    var randomColor =  rgbToHex(getRandomInt(0,255), getRandomInt(0,255), getRandomInt(0,255));
     spnColor.innerHTML = randomColor;
     spnColor.style = 'color:' + randomColor;
 }
-window.addEventListener('DOMContentLoaded',() =>getRandomColor());
-btnRandomColor.addEventListener('click',() =>getRandomColor());
+//event listeners
+window.addEventListener('DOMContentLoaded',() => getRandomColor());
+btnRandomColor.addEventListener('click',() => getRandomColor());
+btnRepo.addEventListener('click',(e) =>{
+if(e.ctrlKey){
+   window.open(ghRepo)
+}else{
+   window.location.href = ghRepo
+}
+})
